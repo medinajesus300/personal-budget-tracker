37916a01e9996e4dfef94d50367fdb6173971f04
jesusmedina <medina.a300@outlook.com>
2024-03-01 12:43:42 -0700

    Add first files to project

---
+++ src/main/java/com/medina/personalbudgettracker/PersonalBudgetTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.medina.personalbudgettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBudgetTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBudgetTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medina/personalbudgettracker/PersonalBudgetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medina.personalbudgettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBudgetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
